2299a4e7e93331e78e1c9b62a3442cc98b20720d
leonard2235 <41180467+leonard2235@users.noreply.github.com>
2018-07-15 16:24:51 +0530

    Update functions.cmake

--- functions/functions.cmake
+++ functions/functions.cmake
@@ -1,6 +1,6 @@
 	cmake_minimum_required(VERSION 2.6)
-	add_executable(	signed src/signed.c	)
-	add_executable(	sizeof2 src/sizeof2.c	)
-	add_executable(	volatile src/volatile.c	)
+	add_executable(	signed C-Programs/functions/src/signed.c	)
+	add_executable(	sizeof2 C-Programs/functions/src/sizeof2.c	)
+	add_executable(	volatile C-Programs/functions/src/volatile.c	)
 
 
